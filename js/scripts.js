ab5f41738e60a14a17337f35fb089ca69cee2719
david rolfs and nick wise <nick@>
2017-05-01 13:30:19 -0700

    add map loop to create new array in all uppercase

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
   $("form#icecream").submit(function(event){
     event.preventDefault();
-    var flavors = $("#flavors").val().split(",")
+    var flavors = $("#flavors").val().split(" ")
 
     //console.log(flavors);
-    flavors.forEach(function(flavor){
+    var upperFlavors = flavors.map(function(flavor){
+      return flavor.toUpperCase();
+    });
+    console.log(upperFlavors)
+    console.log(flavors)
+    upperFlavors.forEach(function(flavor){
       $("ul").append("<li>"+flavor+"</li>")
     })
 
